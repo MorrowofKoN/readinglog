1679250cad2b06ab453a9a9895d09fe2266580f2
MorrowofKoN <65187053+MorrowofKoN@users.noreply.github.com>
2020-11-22 13:37:20 -0600

    edit

---
+++ Monthly_reading_log.sql
@@ -0,0 +1,3 @@
+﻿CREATE TABLE Monthly Reading Logs (id INTEGER PRIMARY KEY, Book TEXT, Date_Started INTEGER, Date_Finished INTEGER, Pages INTEGER);
+INSERT INTO Monthly Reading Logs(0, "The Witches' Book of Self-Care", 11/10/2020, 11/12/2020, 236);
+SELECT * FROM Monthly Reading Logs;
